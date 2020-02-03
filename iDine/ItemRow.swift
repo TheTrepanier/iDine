0bda6fab600f5d5cac907daaaa69a5d1575be15f
Michael Trepanier <mtrepanier91@gmail.com>
2020-02-03 05:42:57 -0700

    item rows created

---
+++ iDine/ItemRow.swift
@@ -0,0 +1,30 @@
+//
+//  ItemRow.swift
+//  iDine
+//
+//  Created by Michael Scott Trepanier on 2/3/20.
+//  Copyright © 2020 Michael Scott Trepanier. All rights reserved.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    var item: MenuItem
+    
+    var body: some View {
+        HStack {
+            Image(item.thumbnailImage)
+            
+            VStack(alignment: .leading) {
+                Text(item.name)
+                Text("$\(item.price)")
+            }
+        }
+    }
+}
+
+struct ItemRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRow(item: MenuItem.example)
+    }
+}
